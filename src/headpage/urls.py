85a9464b8c22e64ab961eca8a3abe1ab52f7cba7
zero0netw0 <108504318+zero0netw0@users.noreply.github.com>
2023-08-05 10:23:59 +0800

    Update urls.py

--- src/headpage/urls.py
+++ src/headpage/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/socials', include('social.urls'))
+    path('/social', include('social.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
